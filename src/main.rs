1da6d042227ca2d4ec417f77f4b450e70fdf2075
Wassim Akkari <was.akkari@gmail.com>
2024-09-30 09:49:23 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![allow(unused)]
+
+#[derive(Debug)]
+struct Rectangle<T, U> {
+    width: T,
+    height: U,
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 1u8,
+        height: 3u16,
+    };
+    println!("rect is {:?}", rect);
+}
+

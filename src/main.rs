ed17362aeb2990fc03ef45f22648e63132755dc7
Wassim Akkari <was.akkari@gmail.com>
2024-09-30 10:48:58 +0100

    Working with boxes for data with unknown size at compile time or with large data that can't be trivially copied arround the stack

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::mem;
+
 #[derive(Debug)]
 struct Rectangle<T, U> {
     width: T,
@@ -22,6 +24,12 @@ fn get_biggest<T: PartialOrd>(a: T, b: T) -> T {
     if a > b { a } else { b }
 }
 
+struct Shuttle {
+    name: String, // 8+8+8 = 24 byte
+    crew_size: u8, // 1 byte
+    propellant: f64, // 8 bytes
+} // 24 + 1 + 8 = 35 bytes after padding (crew_size: u8 +7 bytes) -> 40 bytes
+
 fn main() {
     let rect = Rectangle {
         width: 1u8,
@@ -33,5 +41,21 @@ fn main() {
 
     let biggest = get_biggest(100, 200);
     println!("The biggest value is: {}", biggest);
+
+
+    // Init Shuttle object
+    let vehicle = Shuttle {
+        name: String::from("Atlantis"),
+        crew_size: 7,
+        propellant: 835958.0,
+    };
+    println!("Vehicle size on stack: {} bytes", mem::size_of_val(&vehicle));
+
+    let boxed_vehicle = Box::new(vehicle);
+    println!("Boxed Vehicle size on stack: {} bytes", mem::size_of_val(&boxed_vehicle)); // 8 bytes
+    println!("Boxed Vehicle size on heap: {} bytes", mem::size_of_val(&*boxed_vehicle)); // 40 bytes
+
+    let unboxed_vehicle = *boxed_vehicle;
+    println!("Unboxed Vehicle size on stack: {} bytes", mem::size_of_val(&unboxed_vehicle));
 }
 

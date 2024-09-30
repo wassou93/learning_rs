72cbdc2facf074848cb91caae2de90c18c8d529a
Wassim Akkari <was.akkari@gmail.com>
2024-09-30 11:18:21 +0100

    Add another test condition for boxes_equals and implemented boxes_cmp

--- src/bin/challenge_sum_boxes.rs
+++ src/bin/challenge_sum_boxes.rs
@@ -1,6 +1,6 @@
 /* YOUR CODE GOES HERE */
 
-use std::ops::Add;
+use std::{cmp::Ordering, ops::Add};
 
 
 fn sum_boxes<T: Add<Output = T>>(val1: Box<T>, val2: Box<T>) -> Box<T> {
@@ -11,6 +11,10 @@ fn boxes_equals<T: PartialEq>(val1: Box<T>, val2: Box<T>) -> Box<bool> {
     if *val1 == *val2 { Box::new(true) } else { Box::new(false) }
 }
 
+fn boxes_cmp<T: PartialOrd>(val1: Box<T>, val2: Box<T>) -> Box<Option<Ordering>> {
+    Box::new((*val1).partial_cmp(&*val2))
+}
+
 fn main() {
     let one = Box::new(1);
     let two = Box::new(2);
@@ -18,9 +22,13 @@ fn main() {
 
     let pi = Box::new(std::f64::consts::PI);
     let e = Box::new(std::f64::consts::E);
-    assert_eq!(*sum_boxes(pi, e), std::f64::consts::PI + std::f64::consts::E);
+    assert_eq!(*sum_boxes(pi.clone(), e.clone()), std::f64::consts::PI + std::f64::consts::E);
 
     assert_eq!(*boxes_equals(one.clone(), two.clone()), false);
+    assert_eq!(*boxes_equals(pi.clone(), e.clone()), false);
+
+    assert_eq!(boxes_cmp(one, two).unwrap(), Ordering::Less);
+    assert_eq!(boxes_cmp(pi, e).unwrap(), Ordering::Greater);
 
     println!("Tests passed!");
 }
